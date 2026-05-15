f9572a30123eaddb7aa0b5140b393f4c7df06f8b
Lucas Romero-Wyhnanskyj <lucasr.w101@gmail.com>
2026-05-15 08:52:44 -0400

    normalization functions added

--- internal/utils/normalize.go
+++ internal/utils/normalize.go
@@ -1,1 +1,19 @@
 package utils
+
+import "strings"
+
+func NormalizeMetricName(name string) string { 
+	return strings.ToLower(strings.TrimSpace(name))
+}
+
+func NormalizeMetricValue(value string) string {
+	return strings.TrimSpace(value)
+}
+
+func NormalizeLabels(labels map[string]string) map[string]string {
+	normalized := make(map[string]string)
+	for key, value := range labels {
+		normalized[strings.ToLower(strings.TrimSpace(key))] = strings.TrimSpace(value)
+	}
+	return normalized
+}
