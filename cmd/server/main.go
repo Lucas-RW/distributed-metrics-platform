2440758e9f8f62e1742bc6afce8c7ff368a2e274
Lucas Romero-Wyhnanskyj <lucasr.w101@gmail.com>
2026-08-19 18:15:57 -0400

    deploy service to google cloud run

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -30,6 +30,10 @@ func main() {
     mux := http.NewServeMux()
     mux.HandleFunc("/ingest", handlers.IngestHandler)
     mux.Handle("/metrics", promhttp.Handler())
+    mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+        w.WriteHeader(http.StatusOK)
+        w.Write([]byte("ok"))
+    })
 
     log.Println("Starting server on :8080")
     if err := http.ListenAndServe(":8080", mux); err != nil {
